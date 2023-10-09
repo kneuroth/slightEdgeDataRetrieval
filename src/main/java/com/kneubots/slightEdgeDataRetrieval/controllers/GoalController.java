f4ac50c3d77a6d958a6385773a3395b3107bf56d
Kelly Neuroth <kelly.neuroth@outlook.com>
2023-10-09 17:08:21 -0400

    Adds REST and CRUD functions for Goal

--- src/main/java/com/kneubots/slightEdgeDataRetrieval/controllers/GoalController.java
+++ src/main/java/com/kneubots/slightEdgeDataRetrieval/controllers/GoalController.java
@@ -3,9 +3,8 @@ package com.kneubots.slightEdgeDataRetrieval.controllers;
 import com.kneubots.slightEdgeDataRetrieval.dao.GoalDAO;
 import com.kneubots.slightEdgeDataRetrieval.models.Goal;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -20,8 +19,23 @@ public class GoalController {
         this.goalDAO = goalDAO;
     }
 
-    @GetMapping("")
+    @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Goal> getGoals() throws SQLException {
         return goalDAO.getAllGoals();
+    }
+
+    @PostMapping(path = "",  consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public int create(@RequestBody Goal newGoal) throws SQLException {
+        return goalDAO.insertGoal(newGoal);
+    }
+
+    @DeleteMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public int delete(@PathVariable int id) throws SQLException {
+        return goalDAO.deleteGoal(id);
+    }
+
+    @PutMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public int update(@RequestBody Goal newGoal, @PathVariable int id) throws SQLException {
+        return goalDAO.updateGoal(newGoal, id);
     }
 }
--- src/main/java/com/kneubots/slightEdgeDataRetrieval/dao/GoalDAO.java
+++ src/main/java/com/kneubots/slightEdgeDataRetrieval/dao/GoalDAO.java
@@ -3,6 +3,7 @@ package com.kneubots.slightEdgeDataRetrieval.dao;
 import com.kneubots.slightEdgeDataRetrieval.models.Goal;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.sql.*;
 
@@ -71,5 +72,105 @@ public class GoalDAO {
         }
         disconnect();
         return goals;
+    }
+
+    // Allowed to return more than one but shouldn't
+    public List<Goal> getGoalById(int id) throws SQLException {
+        connect();
+        ArrayList<Goal> goals = new ArrayList<Goal>();
+        try {
+            if (connection != null) {
+                String query = "SELECT * FROM Goal WHERE id=?";
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+                preparedStatement.setInt(1, id);
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                // Process the result set and map goals
+                while (resultSet.next()) {
+                    // Create a Goal object and populate it with data from the result set
+                    Goal goal = new Goal(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            resultSet.getString("description"),
+                            resultSet.getInt("userId"));
+
+                    goals.add(goal);
+                }
+
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        disconnect();
+        return goals;
+    }
+
+    // Returns number of rows modified, should always be 1
+    public int insertGoal(Goal goal) throws SQLException {
+        connect();
+        int modifiedRows = 0;
+        try {
+            if (connection != null) {
+                String query = "INSERT INTO Goal ( name, description, userId) \n" +
+                        "VALUES (?, ?, ?);";
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+                preparedStatement.setString(1, goal.name());
+                preparedStatement.setString(2, goal.description());
+                preparedStatement.setInt(3, goal.userId());
+
+                modifiedRows = preparedStatement.executeUpdate();
+
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        disconnect();
+        return modifiedRows;
+    }
+
+    public int deleteGoal(int id) throws SQLException {
+        connect();
+        int modifiedRows = 0;
+        try {
+            if (connection != null) {
+                String query = "DELETE FROM Goal \n" +
+                        "WHERE id=?;";
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setInt(1, id);
+
+                modifiedRows = preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        disconnect();
+        return modifiedRows;
+    }
+
+
+    public int updateGoal(Goal goal, int id) throws SQLException {
+        connect();
+        int modifiedRows = 0;
+        try {
+            if (connection != null) {
+                String query = "UPDATE Goal\n" +
+                        "SET name = ?, description = ?, userId = ?\n" +
+                        "WHERE id=?;";
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setString(1, goal.name());
+                preparedStatement.setString(2, goal.description());
+                preparedStatement.setInt(3, goal.userId());
+                preparedStatement.setInt(4, id);
+
+                modifiedRows = preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        disconnect();
+        return modifiedRows;
     }
 }
